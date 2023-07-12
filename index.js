92c0af39de9160a1e9bd0cde7b3b97d3e292104a
Maree0 <97540980+Mare024@users.noreply.github.com>
2023-07-12 12:04:15 -0600

    Add files via upload
    new

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const { dbConnection } = require("./source/database/config");
+//Añsignamos a una co nstante  la direccion denuestras rutas 
+const RentaHoteles= require("./source/database/routes/RentaDeHoteles.routes")
 
 dbConnection();
 
 app.use(cors());
+app.use(express.json())
+app.use("/renta",  RentaHoteles)
+
 
 app.get("/", function (_, res) {
   res.send("<h2>Hola mundo</h2>");
